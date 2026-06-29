704a32ee2d676bdef3c4eff51443ad088e7c5001
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-06-29 14:09:14 +0530

    test(pow): add proof-of-work mining tests

--- crates/btc-core/src/block/header.rs
+++ crates/btc-core/src/block/header.rs
@@ -9,7 +9,7 @@ pub struct BlockHeader {
     pub version: u32,
     pub previous_block_hash: BlockHash,
     pub merkle_root: MerkleRoot,
-    pub timestamp: u64,
+    pub timestamp: u32,
     pub bits: u32,
     pub nonce: u32,
 }
@@ -35,13 +35,44 @@ impl BitcoinSerialize for BlockHeader {
     fn serialize(&self) -> Vec<u8> {
         let mut bytes: Vec<u8> = Vec::new();
 
-        bytes .extend_from_slice(&self.version.to_le_bytes());
-        bytes .extend_from_slice(self.previous_block_hash.as_bytes());
-        bytes .extend_from_slice(self.merkle_root.as_bytes());
-        bytes .extend_from_slice(&self.timestamp.to_le_bytes());
-        bytes .extend_from_slice(&self.bits.to_le_bytes());
-        bytes .extend_from_slice(&self.nonce.to_le_bytes());
+        bytes.extend_from_slice(&self.version.to_le_bytes());
+        bytes.extend_from_slice(self.previous_block_hash.as_bytes());
+        bytes.extend_from_slice(self.merkle_root.as_bytes());
+        bytes.extend_from_slice(&self.timestamp.to_le_bytes());
+        bytes.extend_from_slice(&self.bits.to_le_bytes());
+        bytes.extend_from_slice(&self.nonce.to_le_bytes());
 
-        bytes 
+        bytes
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::{block::Block, miner::Miner};
+
+    use super::*;
+
+    #[test]
+    fn modified_header_invalidates_pow() {
+        let mut block = Block {
+            header: BlockHeader {
+                version: 10,
+                previous_block_hash: BlockHash([1u8; 32]),
+                merkle_root: MerkleRoot([2u8; 32]),
+                timestamp: 10000,
+                bits: 0x1f00ffff,
+                nonce: 0,
+            },
+            transactions: vec![],
+        };
+
+        let _res = Miner::mine(&mut block);
+
+        assert!(block.header.verify_pow());
+
+        block.header.version = 11;
+
+        assert!(!block.header.verify_pow())
     }
 }
--- crates/btc-core/src/miner/miner.rs
+++ crates/btc-core/src/miner/miner.rs
@@ -16,3 +16,32 @@ impl Miner {
         }
     }
 }
+
+
+#[cfg(test)] 
+mod test {
+
+    use crate::{block::BlockHeader, types::{BlockHash, MerkleRoot}};
+
+use super::*;
+
+
+#[test]
+fn mines_valid_block() {
+    let mut block = Block {
+        header: BlockHeader {
+            version: 10,
+            previous_block_hash: BlockHash([1u8; 32]),
+            merkle_root: MerkleRoot ([2u8; 32]),
+            timestamp:10000,
+            bits: 0x1f00ffff,
+            nonce:0
+        },
+        transactions: vec![]
+    };
+
+    let res = Miner::mine(&mut block);
+    assert!(res.is_ok());
+    assert!(block.header.verify_pow())
+}
+}
