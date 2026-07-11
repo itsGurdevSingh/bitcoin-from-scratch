a4e168bbc5bf8c2fccd485668010310b6c8407eb
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-07-11 18:07:08 +0530

    feat(unit256): implement bit manipulation methods and division logic for BigUint256

--- crates/btc-core/src/types/unit_256.rs
+++ crates/btc-core/src/types/unit_256.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp::Ordering,
-    ops::{Add, Sub},
+    ops::{Add, AddAssign, Div, DivAssign, Sub, SubAssign},
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -13,6 +13,10 @@ impl From<[u8; 32]> for BigUint256 {
 }
 
 impl BigUint256 {
+    pub fn max() -> Self {
+        BigUint256([255u8; 32])
+    }
+
     pub fn as_bytes(&self) -> &[u8; 32] {
         &self.0
     }
@@ -55,6 +59,61 @@ impl BigUint256 {
         }
 
         unit256
+    }
+
+    /// bit 0 = least-significant bit ,
+    /// bit 255 = most-significant bit
+    pub fn bit(&self, idx: usize) -> bool {
+        let byte = 31 - (idx / 8);
+        let bit = idx % 8;
+
+        (self.0[byte] >> bit) & 1 == 1
+    }
+
+    /// bit 0 = least-significant bit ,
+    /// bit 255 = most-significant bit
+    pub fn set_bit(&mut self, idx: usize) {
+        let byte_offset = idx / 8;
+        let byte = 31 - byte_offset;
+        let bit = idx % 8;
+
+        self.0[byte] |= 1 << bit;
+    }
+
+    pub fn shl1(&mut self) -> bool {
+        let mut carry = 0;
+
+        for i in (0..32).rev() {
+            let new_carry = (self.0[i] >> 7) & 1; // get MSB form byte
+
+            self.0[i] <<= 1; // shift value to left 
+
+            self.0[i] |= carry; // replace LSB with previous byte carry
+
+            carry = new_carry;
+        }
+
+        carry == 1
+    }
+
+    pub fn shr1(&mut self) -> bool {
+        let mut carry = 0;
+
+        for i in 0..32 {
+            let new_carry = self.0[i] & 1; // get LSB form byte
+
+            self.0[i] >>= 1; // shift right by 1 
+
+            self.0[i] |= carry << 7; // put carry as MSB
+
+            carry = new_carry;
+        }
+
+        carry == 1
+    }
+
+    fn zero() -> Self {
+        BigUint256([0u8; 32])
     }
 }
 
@@ -101,18 +160,61 @@ impl Sub for BigUint256 {
 
 impl PartialOrd for BigUint256 {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        {
-            for i in 0..32 {
-                if self.0[i] > other.0[i] {
-                    return Some(Ordering::Greater);
-                }
-
-                if self.0[i] < other.0[i] {
-                    return Some(Ordering::Less);
-                }
+        for i in 0..32 {
+            if self.0[i] > other.0[i] {
+                return Some(Ordering::Greater);
             }
 
-            Some(Ordering::Equal)
+            if self.0[i] < other.0[i] {
+                return Some(Ordering::Less);
+            }
         }
+
+        Some(Ordering::Equal)
+    }
+}
+
+impl SubAssign for BigUint256 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+impl AddAssign for BigUint256 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+impl DivAssign for BigUint256 {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+
+impl Div for BigUint256 {
+    type Output = BigUint256;
+    fn div(self, rhs: Self) -> Self::Output {
+
+        assert!(rhs != BigUint256::zero(), "division by zero");
+
+        let mut dividend = self;
+        let mut quotient = BigUint256::zero();
+        let mut remainder = BigUint256::zero();
+
+        for i in 0..256 {
+            let bit = dividend.shl1();
+
+            remainder.shl1();
+
+            if bit {
+                remainder.set_bit(0);
+            }
+
+            if remainder >= rhs {
+                remainder -= rhs;
+                quotient.set_bit(255 - i);
+            }
+        }
+
+        quotient
     }
 }
